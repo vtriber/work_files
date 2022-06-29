18b120505b6c7c63cda6bd5de0807aeaa8b9cd22
Viktor Triber <vtriber@ya.ru>
2022-06-29 23:59:00 +0300

    Третье задание закончено

--- task_3.py
+++ task_3.py
@@ -1,21 +1,23 @@
 import os
-from pprint import pprint
 
-result_dict = {}
+def merging_files(folder_name):
+    result_dict = {}
 
-for filename in os.listdir("task_3"):
-    line_number = 0
-    line_list = []
-    with open(os.path.join("task_3", filename), 'r') as file:
-        for line in file:
-            line_number += 1
-            line_list.append(line.rstrip('\n'))
+    for filename in os.listdir(folder_name):
+        line_number = 0
+        line_list = []
+        with open(os.path.join(folder_name, filename), 'r') as file:
+            for line in file:
+                line_number += 1
+                line_list.append(line.rstrip('\n'))
 
-        result_dict.update({line_number: {filename: line_list}})
+            result_dict.update({line_number: [filename, line_list]})
 
+    with open("result.txt", 'a') as file:
+        for key, line_dict in sorted(result_dict.items()):
+            file.write(line_dict[0] + '\n')
+            file.write(str(key) + '\n')
+            for line in line_dict[1]:
+                file.write(line + '\n')
 
-pprint(sorted(result_dict.items()))
-with open("result.txt", 'a') as file:
-    for line_dict in result_dict:
-
-            file.write(line)
+merging_files('task_3')
