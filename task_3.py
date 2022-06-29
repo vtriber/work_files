700bb2d9e48b7fdaf27c75cf6cb84ba365da415b
Viktor Triber <vtriber@ya.ru>
2022-06-29 21:57:10 +0300

    Доработан код к третьему заданию

--- task_3.py
+++ task_3.py
@@ -1,6 +1,21 @@
 import os
+from pprint import pprint
+
+result_dict = {}
 
 for filename in os.listdir("task_3"):
+    line_number = 0
+    line_list = []
     with open(os.path.join("task_3", filename), 'r') as file:
-        text = file.read()
-        print(text)
+        for line in file:
+            line_number += 1
+            line_list.append(line.rstrip('\n'))
+
+        result_dict.update({line_number: {filename: line_list}})
+
+
+pprint(sorted(result_dict.items()))
+with open("result.txt", 'a') as file:
+    for line_dict in result_dict:
+        for line_list in line_dict:
+            file.write()
